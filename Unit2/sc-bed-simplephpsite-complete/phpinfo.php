6b2cba41d5d2c4c6a09d63a22ad5f76c9a0e19c4
John Paul Vella <vellj3753@gmail.com>
2024-01-03 20:46:20 +0100

    Unit 2 complete.

---
+++ Unit2/sc-bed-simplephpsite-complete/phpinfo.php
@@ -0,0 +1,16 @@
+<!doctype hyml>
+<html lang="eng">
+
+<?php include 'includes/head.php' ?>
+
+<body>
+    <?php include 'includes/menu.php' ?>
+
+    <div class="container">
+        <?php phpinfo(); ?>
+    </div>
+
+    <?php include 'includes/footer.php' ?>
+</body>
+
+</html>
